ce2dcad355ced212d526fe090c04b507cad7b2da
jini <9765814@gmail.com>
2023-02-02 19:18:37 +0900

    1208

---
+++ app/src/main/java/com/psc/ex_1208/MainActivity.java
@@ -0,0 +1,14 @@
+package com.psc.ex_1208;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
